e2aaa78d2ac489f4b43cd8376ec5e6c16a557a02
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2023-12-28 11:10:38 +0100

    Initial version. Relies on a wordlist stored locally

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WordSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WordSearch");
+
+            if( args.Length == 0 )
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage:");
+                Console.WriteLine("    WordSearch [regex]");
+
+                return;
+            }
+
+            String fileName = @"words_alpha.txt";
+
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead(fileName))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                bool found = false;
+                String line;
+                while ((line =  streamReader.ReadLine()) != null)
+                {
+                    // Process line
+                    if( Regex.Match(line, args[0]).Success )
+                    {
+                        found = true;
+                        Console.WriteLine(line);
+                    }
+                }
+
+                if( !found )
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("No match for: \"" + args[0] + "\"");
+                }
+            }
+        }
+    }
+}
